8869af8277acc818cc1cae116de9961953062349
Joshua Gerrard <joshuagerrard+sort-commit@protonmail.com>
2017-06-03 21:22:21 +0100

    Add another test for already sorted data

--- SortTest/Merge.cpp
+++ SortTest/Merge.cpp
@@ -53,11 +53,36 @@ namespace SortTest
 			// GIVEN: a single element std::vector<TestType>
 			// WHEN: it is sorted
 			// THEN: the return value contains the input element
-			// THEN: the tye of the return value is std::vector<TestType>
+			// THEN: the type of the return value is std::vector<TestType>
 			{
 				auto result = mergeSort(std::vector<TestType>{ {-10}});
 				Assert::IsTrue(result.size() == 1);
 				Assert::IsTrue(result[0] == -10);
+				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+			}
+		}
+
+		TEST_METHOD(AlreadySorted)
+		{
+			// GIVEN: an already sorted std::vector<int32_t>
+			// WHEN: it is sorted
+			// THEN: the return value is sorted
+			// THEN: the type of the return value is std::vector<int32_t>
+			{
+				auto result = mergeSort(std::vector<int32_t>{1, 4, 7, 9, 99});
+				Assert::IsTrue(result.size() == 5);
+				Assert::IsTrue(result == std::vector<int32_t>{1, 4, 7, 9, 99});
+				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+			}
+
+			// GIVEN: an already sorted std::vector<TestType>
+			// WHEN: it is sorted
+			// THEN: the return value is sorted
+			// THEN: the type of the return value is std::vector<TestType>
+			{
+				auto result = mergeSort(std::vector<TestType>{ { 2 }, { 24 }, { 2349 }, { 9121 }});
+				Assert::IsTrue(result.size() == 4);
+				Assert::IsTrue(result == std::vector<TestType>{ { 2 }, { 24 }, { 2349 }, { 9121 }});
 				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
 			}
 		}
