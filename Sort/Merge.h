00bacf671415811bb56eef7546200f398ca6f383
Joshua Gerrard <joshuagerrard+sort-commit@protonmail.com>
2017-06-06 19:38:55 +0100

    I swear I spend my life fighting whitespace

--- Sort/Merge.h
+++ Sort/Merge.h
@@ -3,49 +3,49 @@
 #include <cmath>
 
 namespace Sort {
-	namespace internal {
-		template <typename T>
-		constexpr void mergeSorted(const T* const sourceA, const size_t sourceASize,
-			                       const T* const sourceB, const size_t sourceBSize,
-			                       T* const target) {
-			// can't write directly to target, as it might alias sourceA/sourceB
-			std::vector<T> result(sourceASize + sourceBSize);
+    namespace internal {
+        template <typename T>
+        constexpr void mergeSorted(const T* const sourceA, const size_t sourceASize,
+                                   const T* const sourceB, const size_t sourceBSize,
+                                   T* const target) {
+            // can't write directly to target, as it might alias sourceA/sourceB
+            std::vector<T> result(sourceASize + sourceBSize);
 
-			size_t resultIndex = 0, aIndex = 0, bIndex = 0;
-			while (aIndex < sourceASize && bIndex < sourceBSize) {
-				result[resultIndex++] = sourceA[aIndex] < sourceB[bIndex] ? sourceA[aIndex++] : sourceB[bIndex++];
-			}
+            size_t resultIndex = 0, aIndex = 0, bIndex = 0;
+            while (aIndex < sourceASize && bIndex < sourceBSize) {
+                result[resultIndex++] = sourceA[aIndex] < sourceB[bIndex] ? sourceA[aIndex++] : sourceB[bIndex++];
+            }
 
-			while (aIndex < sourceASize) {
-				result[resultIndex++] = sourceA[aIndex++];
-			}
+            while (aIndex < sourceASize) {
+                result[resultIndex++] = sourceA[aIndex++];
+            }
 
-			while (bIndex < sourceBSize) {
-				result[resultIndex++] = sourceB[bIndex++];
-			}
+            while (bIndex < sourceBSize) {
+                result[resultIndex++] = sourceB[bIndex++];
+            }
 
-			memcpy(target, result.data(), result.size() * sizeof(T));
-		}
-	}
+            memcpy(target, result.data(), result.size() * sizeof(T));
+        }
+    }
 
-	template <typename Container>
-	constexpr Container mergeSort(Container data) {
-		using namespace internal;
+    template <typename Container>
+    constexpr Container mergeSort(Container data) {
+        using namespace internal;
 
-		if (data.size() < 2) {
-			return data;
-		}
+        if (data.size() < 2) {
+            return data;
+        }
 
-		size_t stride = 1;
-		do {
-			for (size_t i = 0; i + stride < data.size(); i += stride * 2) {
-				mergeSorted(&data[i], stride,
-					        &data[i + stride], std::min(stride, data.size() - (i + stride)),
-					        &data[i]);
-			}
-			stride *= 2;
-		} while (stride < data.size());
+        size_t stride = 1;
+        do {
+            for (size_t i = 0; i + stride < data.size(); i += stride * 2) {
+                mergeSorted(&data[i], stride,
+                            &data[i + stride], std::min(stride, data.size() - (i + stride)),
+                            &data[i]);
+            }
+            stride *= 2;
+        } while (stride < data.size());
 
-		return data;
-	}
+        return data;
+    }
 }
--- SortTest/Merge.cpp
+++ SortTest/Merge.cpp
@@ -11,207 +11,207 @@ using namespace Sort;
 
 namespace SortTest
 {		
-	TEST_CLASS(MergeSortTest)
-	{
-	public:
-		TEST_METHOD(NoElements)
-		{
-			// GIVEN: an empty std::vector<int32_t>
-			// WHEN: it is sorted
-			// THEN: the return value is empty
-			// THEN: the type of the return value is std::vector<int>
-			{
-				auto result = mergeSort(std::vector<int32_t> {});
-				Assert::IsTrue(result.size() == 0);
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
+    TEST_CLASS(MergeSortTest)
+    {
+    public:
+        TEST_METHOD(NoElements)
+        {
+            // GIVEN: an empty std::vector<int32_t>
+            // WHEN: it is sorted
+            // THEN: the return value is empty
+            // THEN: the type of the return value is std::vector<int>
+            {
+                auto result = mergeSort(std::vector<int32_t> {});
+                Assert::IsTrue(result.size() == 0);
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
 
-			// GIVEN: an empty std::vector<TestType>
-			// WHEN: it is sorted
-			// THEN: the return value is empty
-			// THEN: the type of the return value is std::vector<TestType>
-			{
-				auto result = mergeSort(std::vector<TestType> {});
-				Assert::IsTrue(result.size() == 0);
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
-			}
-		}
+            // GIVEN: an empty std::vector<TestType>
+            // WHEN: it is sorted
+            // THEN: the return value is empty
+            // THEN: the type of the return value is std::vector<TestType>
+            {
+                auto result = mergeSort(std::vector<TestType> {});
+                Assert::IsTrue(result.size() == 0);
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+            }
+        }
 
-		TEST_METHOD(SingleElement)
-		{
-			// GIVEN: a single element std::vector<int32_t>
-			// WHEN: it is sorted
-			// THEN: the return value contains the input element
-			// THEN: the type of the return value is std::vector<int32_t>
-			{
-				auto result = mergeSort(std::vector<int32_t> {42});
-				Assert::IsTrue(result.size() == 1);
-				Assert::IsTrue(result[0] == 42);
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
+        TEST_METHOD(SingleElement)
+        {
+            // GIVEN: a single element std::vector<int32_t>
+            // WHEN: it is sorted
+            // THEN: the return value contains the input element
+            // THEN: the type of the return value is std::vector<int32_t>
+            {
+                auto result = mergeSort(std::vector<int32_t> {42});
+                Assert::IsTrue(result.size() == 1);
+                Assert::IsTrue(result[0] == 42);
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
 
-			// GIVEN: a single element std::vector<TestType>
-			// WHEN: it is sorted
-			// THEN: the return value contains the input element
-			// THEN: the type of the return value is std::vector<TestType>
-			{
-				auto result = mergeSort(std::vector<TestType>{ {-10}});
-				Assert::IsTrue(result.size() == 1);
-				Assert::IsTrue(result[0] == -10);
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
-			}
-		}
+            // GIVEN: a single element std::vector<TestType>
+            // WHEN: it is sorted
+            // THEN: the return value contains the input element
+            // THEN: the type of the return value is std::vector<TestType>
+            {
+                auto result = mergeSort(std::vector<TestType>{ {-10}});
+                Assert::IsTrue(result.size() == 1);
+                Assert::IsTrue(result[0] == -10);
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+            }
+        }
 
-		TEST_METHOD(AlreadySorted)
-		{
-			// GIVEN: an already sorted std::vector<int32_t>
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<int32_t>
-			{
-				auto result = mergeSort(std::vector<int32_t>{1, 4, 7, 9, 99});
-				Assert::IsTrue(result.size() == 5);
-				Assert::IsTrue(result == std::vector<int32_t>{1, 4, 7, 9, 99});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
+        TEST_METHOD(AlreadySorted)
+        {
+            // GIVEN: an already sorted std::vector<int32_t>
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<int32_t>
+            {
+                auto result = mergeSort(std::vector<int32_t>{1, 4, 7, 9, 99});
+                Assert::IsTrue(result.size() == 5);
+                Assert::IsTrue(result == std::vector<int32_t>{1, 4, 7, 9, 99});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
 
-			// GIVEN: an already sorted std::vector<TestType>
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<TestType>
-			{
-				auto result = mergeSort(std::vector<TestType>{ { 2 }, { 24 }, { 2349 }, { 9121 }});
-				Assert::IsTrue(result.size() == 4);
-				Assert::IsTrue(result == std::vector<TestType>{ { 2 }, { 24 }, { 2349 }, { 9121 }});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
-			}
-		}
+            // GIVEN: an already sorted std::vector<TestType>
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<TestType>
+            {
+                auto result = mergeSort(std::vector<TestType>{ { 2 }, { 24 }, { 2349 }, { 9121 }});
+                Assert::IsTrue(result.size() == 4);
+                Assert::IsTrue(result == std::vector<TestType>{ { 2 }, { 24 }, { 2349 }, { 9121 }});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+            }
+        }
 
-		TEST_METHOD(Backwards)
-		{
-			// GIVEN: a reverse-sorted std::vector<int32_t>
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<int32_t>
-			{
-				auto result = mergeSort(std::vector<int32_t>{ 99, 76, 54, 12, 5, 4, 1});
-				Assert::IsTrue(result.size() == 7);
-				Assert::IsTrue(result == std::vector<int32_t>{1, 4, 5, 12, 54, 76, 99});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
+        TEST_METHOD(Backwards)
+        {
+            // GIVEN: a reverse-sorted std::vector<int32_t>
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<int32_t>
+            {
+                auto result = mergeSort(std::vector<int32_t>{ 99, 76, 54, 12, 5, 4, 1});
+                Assert::IsTrue(result.size() == 7);
+                Assert::IsTrue(result == std::vector<int32_t>{1, 4, 5, 12, 54, 76, 99});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
 
-			// GIVEN: a reverse-sorted std::vector<TestType>
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<TestType>
-			{
-				auto result = mergeSort(std::vector<TestType>{ { 65 }, { 63 }, { 62 }, { 4 }, { 2 }});
-				Assert::IsTrue(result.size() == 5);
-				Assert::IsTrue(result == std::vector<TestType>{ { 2 }, { 4 }, { 62 }, { 63 }, { 65 }});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
-			}
-		}
+            // GIVEN: a reverse-sorted std::vector<TestType>
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<TestType>
+            {
+                auto result = mergeSort(std::vector<TestType>{ { 65 }, { 63 }, { 62 }, { 4 }, { 2 }});
+                Assert::IsTrue(result.size() == 5);
+                Assert::IsTrue(result == std::vector<TestType>{ { 2 }, { 4 }, { 62 }, { 63 }, { 65 }});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+            }
+        }
 
-		TEST_METHOD(Unsorted)
-		{
-			// GIVEN: an unsorted std::vector<int32_t>
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<int32_t>
-			{
-				auto result = mergeSort(std::vector<int32_t>{6, 912, 233, 97, -239234, 0 });
-				Assert::IsTrue(result.size() == 6);
-				Assert::IsTrue(result == std::vector<int32_t>{-239234, 0, 6, 97, 233, 912});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
+        TEST_METHOD(Unsorted)
+        {
+            // GIVEN: an unsorted std::vector<int32_t>
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<int32_t>
+            {
+                auto result = mergeSort(std::vector<int32_t>{6, 912, 233, 97, -239234, 0 });
+                Assert::IsTrue(result.size() == 6);
+                Assert::IsTrue(result == std::vector<int32_t>{-239234, 0, 6, 97, 233, 912});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
 
-			// GIVEN: an unsorted std::vector<TestType>
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<TestType>
-			{
-				auto result = mergeSort(std::vector<TestType>{ { -1 }, { -1230 }, { 2 }, { 659 }});
-				Assert::IsTrue(result.size() == 4);
-				Assert::IsTrue(result == std::vector<TestType>{ { -1230 }, { -1 }, { 2 }, { 659 }});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
-			}
-		}
+            // GIVEN: an unsorted std::vector<TestType>
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<TestType>
+            {
+                auto result = mergeSort(std::vector<TestType>{ { -1 }, { -1230 }, { 2 }, { 659 }});
+                Assert::IsTrue(result.size() == 4);
+                Assert::IsTrue(result == std::vector<TestType>{ { -1230 }, { -1 }, { 2 }, { 659 }});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+            }
+        }
 
-		TEST_METHOD(SomeDuplicate)
-		{
-			// GIVEN: an unsorted std::vector<int32_t> duplicated elements
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<int32_t>
-			{
-				auto result = mergeSort(std::vector<int32_t>{6, 912, 233, 6, 97, 0, -239234, 0 });
-				Assert::IsTrue(result.size() == 8);
-				Assert::IsTrue(result == std::vector<int32_t>{-239234, 0, 0, 6, 6, 97, 233, 912});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
+        TEST_METHOD(SomeDuplicate)
+        {
+            // GIVEN: an unsorted std::vector<int32_t> duplicated elements
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<int32_t>
+            {
+                auto result = mergeSort(std::vector<int32_t>{6, 912, 233, 6, 97, 0, -239234, 0 });
+                Assert::IsTrue(result.size() == 8);
+                Assert::IsTrue(result == std::vector<int32_t>{-239234, 0, 0, 6, 6, 97, 233, 912});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
 
-			// GIVEN: an unsorted std::vector<TestType> with duplicated elements
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<TestType>
-			{
-				auto result = mergeSort(std::vector<TestType>{ { -1 }, { 659 }, { 2 }, { -1230 }, { 2 }, { 659 }});
-				Assert::IsTrue(result.size() == 6);
-				Assert::IsTrue(result == std::vector<TestType>{ { -1230 }, { -1 }, { 2 }, { 2 }, { 659 }, { 659 }});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
-			}
-		}
+            // GIVEN: an unsorted std::vector<TestType> with duplicated elements
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<TestType>
+            {
+                auto result = mergeSort(std::vector<TestType>{ { -1 }, { 659 }, { 2 }, { -1230 }, { 2 }, { 659 }});
+                Assert::IsTrue(result.size() == 6);
+                Assert::IsTrue(result == std::vector<TestType>{ { -1230 }, { -1 }, { 2 }, { 2 }, { 659 }, { 659 }});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+            }
+        }
 
-		TEST_METHOD(AllDuplicate)
-		{
-			// GIVEN: an std::vector<int32_t> of all equal elements
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<int32_t>
-			{
-				auto result = mergeSort(std::vector<int32_t>{ 4, 4, 4, 4 });
-				Assert::IsTrue(result.size() == 4);
-				Assert::IsTrue(result == std::vector<int32_t>{4, 4, 4, 4});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
+        TEST_METHOD(AllDuplicate)
+        {
+            // GIVEN: an std::vector<int32_t> of all equal elements
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<int32_t>
+            {
+                auto result = mergeSort(std::vector<int32_t>{ 4, 4, 4, 4 });
+                Assert::IsTrue(result.size() == 4);
+                Assert::IsTrue(result == std::vector<int32_t>{4, 4, 4, 4});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
 
-			// GIVEN: an std::vector<TestType> of all equal elements
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<TestType>
-			{
-				auto result = mergeSort(std::vector<TestType>{ { -1 }, { -1 }, { -1 }});
-				Assert::IsTrue(result.size() == 3);
-				Assert::IsTrue(result == std::vector<TestType>{ { -1 }, { -1 }, { -1 }});
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
-			}
-		}
+            // GIVEN: an std::vector<TestType> of all equal elements
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<TestType>
+            {
+                auto result = mergeSort(std::vector<TestType>{ { -1 }, { -1 }, { -1 }});
+                Assert::IsTrue(result.size() == 3);
+                Assert::IsTrue(result == std::vector<TestType>{ { -1 }, { -1 }, { -1 }});
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<TestType>>::value);
+            }
+        }
 
-		TEST_METHOD(ManyElements)
-		{
-			// GIVEN: a large std::vector<int32_t> of unsorted data
-			// WHEN: it is sorted
-			// THEN: the return value is sorted
-			// THEN: the type of the return value is std::vector<int32_t>
-			// THEN: the data is sorted sometime this decade
-			{
-				std::vector<int32_t> input;
-				constexpr size_t vectorSize = 1000000;
-				input.resize(vectorSize);
+        TEST_METHOD(ManyElements)
+        {
+            // GIVEN: a large std::vector<int32_t> of unsorted data
+            // WHEN: it is sorted
+            // THEN: the return value is sorted
+            // THEN: the type of the return value is std::vector<int32_t>
+            // THEN: the data is sorted sometime this decade
+            {
+                std::vector<int32_t> input;
+                constexpr size_t vectorSize = 1000000;
+                input.resize(vectorSize);
 
-				// it may not be random, but it's large and unsorted; that's what matters to me
-				for (size_t i = 0; i < vectorSize; ++i) {
-					input[i] = vectorSize / (i + 1) + i * (i * i % 10);
-				}
+                // it may not be random, but it's large and unsorted; that's what matters to me
+                for (size_t i = 0; i < vectorSize; ++i) {
+                    input[i] = vectorSize / (i + 1) + i * (i * i % 10);
+                }
 
-				std::vector<int32_t> expected = input;
-				std::sort(expected.begin(), expected.end());
+                std::vector<int32_t> expected = input;
+                std::sort(expected.begin(), expected.end());
 
-				auto result = mergeSort(input);
-				Assert::IsTrue(result.size() == vectorSize);
-				Assert::IsTrue(result == expected);
-				Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
-			}
-		}
-	};
+                auto result = mergeSort(input);
+                Assert::IsTrue(result.size() == vectorSize);
+                Assert::IsTrue(result == expected);
+                Assert::IsTrue(std::is_same<decltype(result), std::vector<int32_t>>::value);
+            }
+        }
+    };
 }
